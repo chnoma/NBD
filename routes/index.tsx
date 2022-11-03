a8fcb1bfa3dc333a942201af3cb3969b5ce3cc2e
Fresh Server <adrianarnett0@gmail.com>
2022-11-03 22:37:14 +0000

    Test commit via SSH
    Co-authored-by: Tyler Mueller <muelltyl@gmail.com>

--- routes/index.tsx
+++ routes/index.tsx
@@ -14,8 +14,9 @@ export default function Home() {
           alt="the fresh logo: a sliced lemon dripping with juice"
         />
         <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
+          Welcome to `arch`. Try updating this message in the ./routes/index.tsx
           file, and refresh.
+          Hello this is the index
         </p>
         <Counter start={3} />
       </div>
