342d872279461957c396d4f86d65e7002a1570bf
Tyler Mueller <muelltyl@gmail.com>
2022-11-05 21:07:43 -0500

    removed unused variables

--- routes/api/git/webhook.ts
+++ routes/api/git/webhook.ts
@@ -1,7 +1,7 @@
 import { HandlerContext } from "$fresh/server.ts";
-import { fetch, getHashes, pull } from '$src/git/mod.ts';
+import { fetch, pull } from '$src/git/mod.ts';
 
-export const handler = async (req: Request, ctx: HandlerContext): Promise<Response> => {
+export const handler = async (req: Request, _ctx: HandlerContext): Promise<Response> => {
     if(req.headers.has('User-agent') ? !req.headers.get('User-agent')?.includes('GitHub-Hookshot/') : true)
         return new Response(null, { status: 401 });
     await fetch();
