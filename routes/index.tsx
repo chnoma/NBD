747c6d170b413e78fe43fb8f4dc918f0a3d8af38
Adrian Arnett <adrianarnett0@gmail.com>
2022-11-05 20:57:56 -0500

    shader test

--- routes/index.tsx
+++ routes/index.tsx
@@ -9,12 +9,12 @@ export default function Home() {
         <link href="./css/index.css" rel="stylesheet"></link>
         <link href="./css/fonts.css" rel="stylesheet"></link>
       </Head>
+      <iframe src="https://www.shadertoy.com/embed/Xtt3R4?gui=true&t=10&paused=true&muted=false" width="640" height="360" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
       <div class="header">
         <div class="title">
           <h1>Nodesheet</h1>
           <p>It's a spreadsheet, but with nodes!</p>
         </div>
-        <iframe allowtransparence="true" width="640" height="360" frameborder="0" src="https://www.shadertoy.com/embed/Xtt3R4?gui=false&t=10&paused=false&muted=false" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
       </div>
     </>
   );
