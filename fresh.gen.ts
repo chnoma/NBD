2b0688776c6d264d3fe13d4dad0a2056ec747118
Tyler Mueller <muelltyl@gmail.com>
2022-11-05 20:47:45 -0500

    modifies index page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,22 +5,20 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/api/git/commits/latest.ts";
-import * as $2 from "./routes/api/git/pull.ts";
-import * as $3 from "./routes/api/git/webhook.ts";
-import * as $4 from "./routes/api/joke.ts";
-import * as $5 from "./routes/api/math/add.ts";
-import * as $6 from "./routes/index.tsx";
+import * as $2 from "./routes/api/git/webhook.ts";
+import * as $3 from "./routes/api/joke.ts";
+import * as $4 from "./routes/api/math/add.ts";
+import * as $5 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
     "./routes/api/git/commits/latest.ts": $1,
-    "./routes/api/git/pull.ts": $2,
-    "./routes/api/git/webhook.ts": $3,
-    "./routes/api/joke.ts": $4,
-    "./routes/api/math/add.ts": $5,
-    "./routes/index.tsx": $6,
+    "./routes/api/git/webhook.ts": $2,
+    "./routes/api/joke.ts": $3,
+    "./routes/api/math/add.ts": $4,
+    "./routes/index.tsx": $5,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
