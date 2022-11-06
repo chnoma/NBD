3cc309426f5c436ceec7a09bc65e8a1bd950a852
Adrian Arnett <adrianarnett0@gmail.com>
2022-11-05 20:16:00 -0500

    bushed

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <Head>
         <title>Fresh App</title>
       </Head>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/ZiVziYIIwdA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/watch?v=1HZ3Tjohwqo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </>
   );
 }
