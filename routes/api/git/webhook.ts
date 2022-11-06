2129052db8e7acee678407b770b4a19cfa1a7b1d
Tyler Mueller <muelltyl@gmail.com>
2022-11-05 20:02:09 -0500

    reformats git webhook for auto pull

---
+++ routes/api/git/webhook.ts
@@ -0,0 +1,10 @@
+import { HandlerContext } from "$fresh/server.ts";
+import { fetch, getHashes, pull } from '$src/git/mod.ts';
+
+export const handler = async (req: Request, ctx: HandlerContext): Promise<Response> => {
+    if(req.headers.has('User-agent') ? !req.headers.get('User-agent')?.includes('GitHub-Hookshot/') : true)
+        return new Response(null, { status: 401 });
+    await fetch();
+    await pull();
+    return new Response(null,{ status: 200 });
+}
--- src/git/pull.ts
+++ src/git/pull.ts
@@ -1,4 +1,4 @@
 
 export async function pull(): Promise<void> {
-    await (Deno.run({ cmd: ['git', 'pull'] })).status();
+    await (Deno.run({ cmd: ['git', 'pull', '--force'] })).status();
 }
